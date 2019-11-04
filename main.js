f9ad71327b85b7e8732304258d3e4617a0cc43a1
Johanna <siven.johanna@gmail.com>
2019-11-04 12:28:11 +0100

    increase timer from 30 to 60 sec

--- main.js
+++ main.js
@@ -1,64 +1,64 @@
 let playing = false;
 
 function startGame() {
-    playing = true;
-    document.querySelector("button").style.visibility = "hidden";
-    let counter = 30;
-    timer = document.querySelector('.timer');
-    timer.innerText = counter;
+  playing = true;
+  document.querySelector("button").style.visibility = "hidden";
+  let counter = 60;
+  timer = document.querySelector(".timer");
+  timer.innerText = counter;
 
-    const interval = setInterval(function () {
-        counter--;
-        if (counter <= 0) {
-            clearInterval(interval);
-            playing = false;
-            setTimeout(function () {
-                window.location.reload()
-            }, 5000);
-            document.querySelector("button").style.visibility = "visible";
-        }
-        timer.innerText = counter;
-    }, 1000);
+  const interval = setInterval(function() {
+    counter--;
+    if (counter <= 0) {
+      clearInterval(interval);
+      playing = false;
+      setTimeout(function() {
+        window.location.reload();
+      }, 5000);
+      document.querySelector("button").style.visibility = "visible";
+    }
+    timer.innerText = counter;
+  }, 1000);
 }
 const game = new Game();
 
 function preload() {
-    game.gamePreload();
+  game.gamePreload();
 }
 
 function setup() {
-    let canvas = createCanvas(1000, 800);
-    canvas.parent('canvas');
-    game.gameSetup();
+  let canvas = createCanvas(1000, 800);
+  canvas.parent("canvas");
+  game.gameSetup();
 }
 
 function draw() {
-    if (!playing) {
-        if (!game.player1Score && !game.player2Score) {
-            return;
-        }
-        if (game.player1Score > game.player2Score) {
-            textSize(35);
-            fill("white")
-            text("Game over - Player 1 wins!", 300, 350);
-        } else if (game.player2Score > game.player1Score) {
-            textSize(35);
-            fill("white")
-            text("Game over - Player 2 wins!", 300, 350);
-        } else {
-            textSize(35);
-            fill("white")
-            text("Game over - Draw", 370, 350);
-        }
+  if (!playing) {
+    if (!game.player1Score && !game.player2Score) {
+      return;
     }
-    playing && game.gameDraw();
+    if (game.player1Score > game.player2Score) {
+      textSize(35);
+      fill("white");
+      text("Game over - Player 1 wins!", 300, 350);
+    } else if (game.player2Score > game.player1Score) {
+      textSize(35);
+      fill("white");
+      text("Game over - Player 2 wins!", 300, 350);
+    } else {
+      textSize(35);
+      fill("white");
+      text("Game over - Draw", 370, 350);
+    }
+  }
+  playing && game.gameDraw();
 }
 
 function keyPressed() {
-    if (keyCode === 32) {
-        game.player2.playerJump();
-    } else if (keyCode == 16) {
-        game.player1.playerJump();
-    }
-    return false;
+  if (keyCode === 32) {
+    game.player2.playerJump();
+  } else if (keyCode == 16) {
+    game.player1.playerJump();
+  }
+  return false;
 }
